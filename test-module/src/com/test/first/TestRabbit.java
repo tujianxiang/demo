59a18cca68bb14d12fe94b4fa87bed9a43a43369
tujianxiang <979416589@qq.com>
2019-08-17 17:48:34 +0800

    修改兔子类

--- test-module/src/com/test/first/TestRabbit.java
+++ test-module/src/com/test/first/TestRabbit.java
@@ -4,6 +4,7 @@ public class TestRabbit {
     public static void main(String[] args) {
         int num = caculatRabbit(8);
         System.out.println(num);
+        System.out.println("兔子数量");
     }
 
     public static int caculatRabbit(int i){
